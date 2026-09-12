09e23baca6a33a31a54330b3cf19ba4b12433f9f
NeneRoyalFans <kenginnotech2@gmail.com>
2026-09-12 18:52:35 +0700

    Add files via upload

--- contact-config.js
+++ contact-config.js
@@ -1,1 +1,1 @@
-window.AIBOX_CONTACT_ENDPOINT = "PASTE_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE";
+window.AIBOX_CONTACT_ENDPOINT = "https://script.google.com/macros/s/AKfycbxBeiHMhyMlQkI6QYEULsDI9so0oqC4lal77IkZ9yZ9djUkEfKZzUVvzxC4DOaop6aq/exec";
